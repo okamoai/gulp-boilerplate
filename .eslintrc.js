ea88e3c3c82075f3ed8dea6d475b5d2af5e073a5
okamoai <okamoai@rano-raraku.net>
2018-06-30 16:25:00 +0900

    rename eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ['airbnb/base'],
+  rules: {
+    semi: ['error', 'never'],
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
+    'import/no-extraneous-dependencies': ['off'],
+    'no-new': 'off',
+  },
+}
